8060c6eec4ee11d55a887ecc8a827019859f7828
Johnnysky <vanpasie20@gmail.com>
2025-12-15 04:45:13 -0800

    checking out a new api

--- src/Components/VerifyPage.jsx
+++ src/Components/VerifyPage.jsx
@@ -27,7 +27,7 @@ export default function VerifyPage() {
     setLoading(true);
     try {
       const response = await fetch(
-        "https://chat-backend-xt3z.onrender.com/api/auth/send-otp",
+        "express-js-on-vercel-eight-virid.vercel.app/api/auth/send-otp",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
